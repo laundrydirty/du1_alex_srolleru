913f76df82a29c467b3962202368c3dbfd63c55a
Alex <a.sroller@seznam.cz>
2019-11-20 22:27:18 +0100

    verze1

--- 1.py
+++ 1.py
@@ -1,34 +1,33 @@
-from math import sin,tan,pi,e,log
+from math import sin, tan, pi, e, log
 
-z=input("Jaké zobrazení: ")
-m=int(input("Jaké měřítko: "))
+z = input("Jaké zobrazení: ")
+m = int(input("Jaké měřítko: "))
 
-#osetreni meritka
+# osetreni meritka
 if m <= 0:
     print("špatný formát měřítka, zkus to prosím znova")
     exit()
-#volitelny polomer
-r=float(input("Zadáš i poloměr?:"))
+# volitelny polomer
+r = float(input("Zadáš i poloměr?:"))
 
-#pokud uzviatel zada 0, bude pouzit prednastaveny polomer
+# pokud uzviatel zada 0, bude pouzit prednastaveny polomer
 if r == 0:
-    r=6371.11
+    r = 6371.11
 
-#pokud uzivatel zada kladne cislo, bude pouzit jeho vstup
-elif r>0:
-    r=r
+# pokud uzivatel zada kladne cislo, bude pouzit jeho vstup
+elif r > 0:
+    r = r
 
-#pokud uzivatel zada neco jineho, vyskoci chybova hlaska
+# pokud uzivatel zada neco jineho, vyskoci chybova hlaska
 else:
     print("špatný formát poloměru, zkus to prosím znova")
     exit()
 
+# seznamy rovnobezek a poledniku, kam budou ukladany hodnoty
+rovno = []
+pole = []
 
-#seznamy rovnobezek a poledniku, kam budou ukladany hodnoty
-rovno=[]
-pole=[]
-
-#poledniky pro vsechna zobrazeni stejna
+# poledniky pro vsechna zobrazeni stejna
 for i in range(37):
     x = r * ((-180 + (i * 10)) * pi / 180)
     x = x * 100000 / m
@@ -38,7 +37,7 @@ for i in range(37):
     else:
         pole.append(x)
 
-#rovnobezky pro zobrazeni
+# rovnobezky pro zobrazeni
 if z == 'A':
     for j in range(19):
         y = r * ((-90 + (j * 10)) * pi / 180)
@@ -72,13 +71,13 @@ elif z == 'B':
 elif z == 'M':
     for j in range(19):
         if j == 9:
-            y=200
-        elif j<9:
-            y = r * log(1/tan((90 - (j * 10)) * pi / 360),e)
+            y = 200
+        elif j < 9:
+            y = r * log(1 / tan((90 - (j * 10)) * pi / 360), e)
             y = y * 100000 / m
             y = round(-y, 1)
         else:
-            y = r * log(1/tan((-90 + (j * 10)) * pi / 360),e)
+            y = r * log(1 / tan((-90 + (j * 10)) * pi / 360), e)
             y = y * 100000 / m
             y = round(y, 1)
 
@@ -87,13 +86,13 @@ elif z == 'M':
         else:
             rovno.append(y)
 
-#pokud uzivatel zada neco jineho, vyskoci chybova hlaska
+# pokud uzivatel zada neco jineho, vyskoci chybova hlaska
 else:
     print("toto zobrazení neznám, prosím zkus to znovu")
     exit()
 
-#vypise vysledek
-print("Rovnoběžky: ",rovno)
-print("Poledníky: ",pole)
+# vypise vysledek
+print("Rovnoběžky: ", rovno)
+print("Poledníky: ", pole)
 
-    
+
